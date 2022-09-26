f2d0e82b5f345c36f8697d06d7589e9699a63949
landisal <landisal@mail.uc.edu>
2022-09-26 19:51:20 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
+        double itemPrice = 100.74;
+        double tax = ((0.05 * itemPrice)+itemPrice);
+
+        System.out.println("The Price of the items is " +itemPrice);
+        System.out.println("Price of Items with Tax is : " +tax);
+
     }
 }
